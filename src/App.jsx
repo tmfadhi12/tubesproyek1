c91b2776d7c344dccb19a12bcad502e9d3dd8341
tmfadhli 12 <thoriqmf123@gmail.com>
2023-05-04 07:24:42 +0700

    test

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Routing from './Routing';
+import React from 'react';
+
+function App() {
+  return (
+   <React.Fragment>
+      <Routing/>
+   </React.Fragment>
+  );
+}
+
+export default App;
