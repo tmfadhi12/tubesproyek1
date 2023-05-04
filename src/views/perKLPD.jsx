8418376544f5af6c3e6da45e8ce83824531443e0
tmfadhli 12 <thoriqmf123@gmail.com>
2023-05-04 07:52:32 +0700

    ui

--- src/views/perKLPD.jsx
+++
@@ -1,9 +0,0 @@
-const PerKLPD = () => {
-    return(
-        <div>
-            <iframe title="proyek1" width="1140" height="541.25" src="https://app.powerbi.com/reportEmbed?reportId=c2cfa353-ce68-405c-834e-8735371d47b1&autoAuth=true&ctid=98374cd7-c8b8-413c-8476-b8722ae7e8e8" frameborder="0" allowFullScreen="true"></iframe>
-        </div>
-    )
-}
-
-export default PerKLPD
